2b5dad91e6dce9c5803e0fa731797f1c0838aa28
Deal-overcomer <deal.overcomer@gmail.com>
2025-08-03 08:56:29 +0300

    added gemini's models

--- app/constants/Options.ts
+++ app/constants/Options.ts
@@ -1,7 +1,9 @@
 export const englishLevels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 export const geminiModels = [
+  'gemini-2.0-flash-lite',
   'gemini-2.0-flash',
   'gemini-2.0-pro',
+  'gemini-2.5-flash-lite',
   'gemini-2.5-flash',
   'gemini-2.5-pro',
 ];
